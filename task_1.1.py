66a39b6d3d11129bbb1b9dbf68c0bfb9c2325680
лора <timkas875@gmail.com>
2022-06-12 16:26:10 +1000

    Initial Commit

---
+++ task_1.1.py
@@ -0,0 +1,5 @@
+number = input('Enter two-digit number')
+name = input('Enter the name of the 16th world chess champion')
+print(number)
+print(name)
+
---
+++ task_5.py
@@ -0,0 +1,9 @@
+
+revenue = int(input('Enter the company revenue'))
+costs = int(input('Enter the company costs'))
+
+if revenue > costs:
+    print('Profit')
+else:
+    print('Loss')
+
