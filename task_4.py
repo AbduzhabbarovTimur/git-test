a0b32e1f0fd533167b1a974c7dc20352eb566741
лора <timkas875@gmail.com>
2022-06-13 09:24:36 +1000

    Initial Commit

--- task_4.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Enter a positive integer'))
-m = a % 10
-a = a//10
-
-while a > 0:
-    if a % 10 > m:
-        m = a % 10
-        a = a//10
-print(m)
--- task_5.py
+++
@@ -1,8 +0,0 @@
-revenue = int(input('Enter the company revenue'))
-costs = int(input('Enter the company costs'))
-
-if revenue > costs:
-    print('Profit')
-else:
-    print('Loss')
-
