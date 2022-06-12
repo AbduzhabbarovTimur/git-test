955457fe1d780994d0079499a51b538dcf76de8a
лора <timkas875@gmail.com>
2022-06-13 09:45:12 +1000

    Initial Commit

--- task_1.1.py
+++ task_1.1.py
@@ -1,3 +1,5 @@
+# Задача 1.1 Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+
 number = input('Enter two-digit number')
 name = input('Enter the name of the 16th world chess champion')
 print(number)
