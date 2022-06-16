eb1c4e073b0e0e36a78cbfb68164b46534cfb479
Тимур Абдужаббаров <97503766+AbduzhabbarovTimur@users.noreply.github.com>
2022-06-16 16:54:21 +1000

    Delete task_1.1.py

--- task_1.1.py
+++
@@ -1,7 +0,0 @@
-# Задача 1.1 Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
-
-number = input('Enter two-digit number')
-name = input('Enter the name of the 16th world chess champion')
-print(number)
-print(name)
-
